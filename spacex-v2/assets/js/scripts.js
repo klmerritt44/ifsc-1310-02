6efaf56b9431bc08ed8945f5740e9997f9b97a89
klmerritt44 <143474174+klmerritt44@users.noreply.github.com>
2023-12-07 17:54:09 +0000

    added image slider

---
+++ spacex-v2/assets/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('.hero').slick({
+      dots: true,
+      infinite: true,
+      speed: 900,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+});
